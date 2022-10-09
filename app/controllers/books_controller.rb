be5bf908b36fb4c1d9b6f74df98047cd4c581240
EC2 Default User <ec2-user@ip-172-31-1-67.ap-northeast-1.compute.internal>
2022-10-09 10:49:36 +0000

    [Bookers] 7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,47 +2,47 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
-  end
-  
-  def new
     @book = Book.new
   end
   
   def create
-    book = Book.new(books_params)
-    if book.save
-      flash[:notice] = "ブックが正常に作成されました。"
-      redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
   
   def show
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
   
-  def update #設定
-    book = Book.find(params[:id])
-    if book.update(books_params)
-    redirect_to book_path(book.id)
+  def update 
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
-      render :edit
+      render("books/edit")
     end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
   
   private
-  def books_params
-    params.permit(:title, :body)
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
